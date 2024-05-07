e55cb391df84bb8813524447d591785fe37c86cd
mel-am <122634403+mel-am@users.noreply.github.com>
2024-05-08 00:04:41 +0100

    Server

--- server/server.js
+++ server/server.js
@@ -6,12 +6,14 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const connectionString = process.env.DATABASE_URL;
 
-const db = new pg.Pool({ connectionString: connectionString });
 
 app.use(cors());
 app.use(express.json());
+const connectionString = process.env.DATABASE_URL;
+const db = new pg.Pool({ connectionString: connectionString });
+
+
 
 app.get("/", (_request, response) => {
   response.json("Best TV Shows");
@@ -23,27 +25,42 @@ app.get("/actors", async (_request, response) => {
 });
 
 
-// });
 app.get("/shows", async (_request, response) => {
   const result = await db.query("SELECT * FROM shows;");
   response.json(result.rows);
 });
 
-app.post("/posts", async (_request, response) => {
-  const username = _request.body.username;
-  const location = _request.body.location;
-  const actor =  _request.body.actor;
-  const show =  _request.body.show;
-  const post = _request.body.post;
+app.post("/posts", async (req, res) => {
+  console.log("req.body", req.body);
 
 
-  await db.query(`INSERT INTO posts (username, location, actor, show, post) VALUES ($1, $2, $3, $4, $5)`, [
+  const username = req.body.username;
+  const location = req.body.location;
+  const actor =  req.body.actor;
+  const show =  req.body.show;
+  const post = req.body.post;
+
+  // app.post("/posts", async (_request, _response) => {
+  // const username = _request.body.username;
+  // const location = _request.body.location;
+  // const actor =  _request.body.actor;
+  // const show =  _request.body.show;
+  // const post = _request.body.post;
+
+
+  db.query(`INSERT INTO posts (username, location, actor, show, post) VALUES ($1, $2, $3, $4, $5)`, [
     username,
     location,
     actor,
     show,
     post
   ]);
-  response.json({ success: true });
+  response.json({ status: "Post received!" });
+});
+
+app.get('/posts', async (_req, res) => {
+  const { rows } = await pool.query('SELECT * FROM posts');
+  res.json(rows);
+  
 });
 app.listen(8080, () => console.log(" 8080 baby!"));
